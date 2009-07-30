466b319c3bc1c7217d93c1b1af81ca24a6f81e7c
James Rosen <james.a.rosen@gmail.com>
2009-07-30 12:14:14 -0400

    added validation that :cas_server is supplied

--- lib/casrack_the_authenticator/simple.rb
+++ lib/casrack_the_authenticator/simple.rb
@@ -4,10 +4,27 @@ module CasrackTheAuthenticator
     
     def initialize(app, options)
       @app = app
+      @options = options
+      raise ArgumentError.new(":cas_server is a required option") if options[:cas_server].nil?
     end
     
     def call(env)
-      @app.call(env)
+      response = @app.call(env)
+      redirect_on_401(response)
+    end
+    
+    private
+    
+    def redirect_on_401(response)
+      if response[0] == 401
+        redirect_to_cas
+      else
+        response
+      end
+    end
+    
+    def redirect_to_cas
+      [ 302, { 'Location' => @options[:cas_server] }, [] ]
     end
     
   end
--- test/simple_test.rb
+++ test/simple_test.rb
@@ -2,11 +2,19 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 
 class SimpleTest < Test::Unit::TestCase
   
+  context 'creating a Simple authenticator' do
+    should 'require a :cas_server' do
+      assert_raises(ArgumentError) do
+        CasrackTheAuthenticator::Simple.new(:anything, {})
+      end
+    end
+  end
+  
   context 'a Simple authenticator' do
     
     setup do
       @app = Object.new
-      @authenticator = CasrackTheAuthenticator::Simple.new(@app, {})
+      @authenticator = CasrackTheAuthenticator::Simple.new(@app, {:cas_server => 'http://cas.test/'})
     end
     
     context 'when receiving a 200 from below' do
