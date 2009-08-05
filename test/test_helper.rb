9866ac7ecb80294b27521e08f5f78edee37691e8
James Rosen <james.a.rosen@gmail.com>
2009-08-05 16:19:54 -0400

    added redgreen for test output coloring

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require 'rubygems'
 require 'shoulda'
 gem 'jferris-mocha'
 require 'mocha'
+require 'redgreen'
 
 lib_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
 $: << lib_path unless $:.include?(lib_path)
