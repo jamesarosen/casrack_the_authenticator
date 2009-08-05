2f368bd19c38260e12413e760f0351ee3beb9cb1
James Rosen <james.a.rosen@gmail.com>
2009-08-05 16:20:26 -0400

    ignored old LIBXML2 warning

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,8 @@ gem 'jferris-mocha'
 require 'mocha'
 require 'redgreen'
 
+I_KNOW_I_AM_USING_AN_OLD_AND_BUGGY_VERSION_OF_LIBXML2 = 1
+
 lib_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
 $: << lib_path unless $:.include?(lib_path)
 
